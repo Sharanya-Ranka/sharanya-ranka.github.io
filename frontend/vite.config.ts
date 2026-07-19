055a0ab3942899855f03d6e4af32bb5e5999d405
Sharanya Ranka <sharanya.ranka@gmail.com>
2026-07-19 20:01:54 +0530

    Try generic base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/sharanya-ranka.github.io/',
+  base: '/',
 })
