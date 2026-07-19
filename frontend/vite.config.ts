939201ecb792efcf50e40de76520081cb2a2487e
Sharanya Ranka <sharanya.ranka@gmail.com>
2026-07-19 18:24:59 +0530

    Adding deployment workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/sharanya-ranka.github.io/',
 })
